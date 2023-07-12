e743180d36d17d09b3b7a756ba60eb9bcdcc9aa4
EmmaTheVibe <peteremmanuel316@gmail.com>
2023-07-12 19:24:14 +0100

    test

--- script.js
+++ script.js
@@ -1,48 +1,91 @@
 const buttons = document.querySelectorAll(".btn")
+const link1 = document.querySelector('#link1')
+const link2 = document.querySelector('#link2')
+const link3 = document.querySelector('#link3')
+const img1 = document.querySelector("#iphone1")
+const img2 = document.querySelector("#iphone2")
+const img3 = document.querySelector("#iphone3")
+const heading = document.querySelector(".heading")
+const receive = document.querySelector(".receive")
+const transfer = document.querySelector(".transfer")
+const fund = document.querySelector(".fund")
+
 buttons.forEach(active => {
-    const link1 = document.querySelector('#link1')
-    const link2 = document.querySelector('#link2')
-    const link3 = document.querySelector('#link3')
-    const img1 = document.querySelector("#iphone1")
-    const img2 = document.querySelector("#iphone2")
-    const img3 = document.querySelector("#iphone3")
     active.addEventListener("click", () => {
         document.querySelector('.clicked')?.classList.remove('clicked')
         active.classList.add('clicked') 
-         
         if(link2.classList.contains("clicked")){
             img1.style.display = "none"
             img2.style.display = "unset"
             img3.style.display = "none"
-    
-            const transferText = document.querySelector(".heading")
-            transferText.innerText = "Transfer Money"
-            
-            const transferPrg = document.querySelector(".prg")
-            transferPrg.innerText = "No more worries about delayed or failed transactions. Parkway Wallet enables you to send money effortlessly to any bank of your choice. With just a few taps, you can transfer funds quickly and safely, allowing you to fulfil your financial obligations without any hassles. Enjoy peace of mind knowing that your transactions are completed smoothly and promptly."
-        } else if(link3.classList.contains("clicked")){
+        
+            heading.innerText = "Transfer Money"
+            receive.style.display = "none"
+            transfer.style.display = "unset"
+            fund.style.display = "none"
+        } 
+        if(link3.classList.contains("clicked")){
             img1.style.display = "none"
             img2.style.display = "none"
             img3.style.display = "unset"
-    
-            const transferText = document.querySelector(".heading")
-            transferText.innerText = "Fund Wallet"
-            
-            const transferPrg = document.querySelector(".prg")
-            transferPrg.innerText = "Instantly top-up your wallet using your card, USSD codes, or a simple bank transfer. Parkway Wallet ensures that your funds are readily available whenever needed, enabling you to make seamless payments and enjoy uninterrupted services."
-        } else if(link1.classList.contains("clicked")){
+        
+            heading.innerText = "Fund Wallet"
+            receive.style.display = "none"
+            transfer.style.display = "none"
+            fund.style.display = "unset"
+        } 
+        if(link1.classList.contains("clicked")){
             img1.style.display = "unset"
             img2.style.display = "none"
             img3.style.display = "none"
-            
-            const transferText = document.querySelector(".heading")
-            transferText.innerText = "Receive Money"
-            
-            const transferPrg = document.querySelector(".prg")
-            transferPrg.innerText = "Say goodbye to complicated payment methods and delays. Now, you can easily receive money instantly from anyone, anywhere. Simplify your payment collection with Parkway Wallet using a unique account ID created."
+        
+            heading.innerText = "Receive Money"
+            receive.style.display = "unset"
+            transfer.style.display = "none"
+            fund.style.display = "none"
         }  
     })
+     
 })
+// link1.addEventListener("click", () => {
+//     img1.style.display = "unset"
+//     img2.style.display = "none"
+//     img3.style.display = "none"
+
+//     // heading.innerText = "Receive Money"
+//     receive.style.display = "unset"
+//     transfer.style.display = "none"
+//     fund.style.display = "none"
+// })
+// link2.addEventListener("click", () => {
+//     img1.style.display = "none"
+//     img2.style.display = "unset"
+//     img3.style.display = "none"
+        
+//     // heading.innerText = "Transfer Money"
+//     receive.style.display = "none"
+//     transfer.style.display = "unset"
+//     fund.style.display = "none"
+// })
+// link3.addEventListener("click", () => {
+//     img1.style.display = "none"
+//     img2.style.display = "none"
+//     img3.style.display = "unset"
+        
+//     heading.innerText = "Fund Wallet"
+//     receive.style.display = "none"
+//     transfer.style.display = "none"
+//     fund.style.display = "unset"
+// })
+
+
+// for(let button of buttons){
+//     button.addEventListener("click", () => {
+//         document.querySelector('.clicked')?.classList.remove('clicked')
+//         active.classList.add('clicked') 
+//     })
+// }
+
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
